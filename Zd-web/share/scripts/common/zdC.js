35accc96cf98140537102a963c4d9d145de52547
Brent Roberts <broberts@zendesk.com>
2014-01-07 21:13:43 +0000

    More intro progress.  JS and CSS

--- Zd-web/share/scripts/common/zdC.js
+++ Zd-web/share/scripts/common/zdC.js
@@ -1,14 +1,12 @@
 function zdC(tag,a) {
 	var e = document.createElement(tag);
-	
-	if( tag == "img" )
-		e.src = a;
 
-	if( a ) {
-		if( tag == "div" ) {
+	if( a ) {	
+		if( tag == "img" ){
+			e.src = a;
+		}else{
 			e.className = a;
 		}
-	}	
-
+	}
 	return e;
 }
