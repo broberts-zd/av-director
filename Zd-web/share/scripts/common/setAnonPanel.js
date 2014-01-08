7485d8a302a66e0374df79a1b14a8f54f44c64cb
Brent Roberts <broberts@zendesk.com>
2014-01-08 03:05:41 +0000

    removed extraneous files

--- Zd-web/share/scripts/common/setAnonPanel.js
+++
@@ -1,6 +0,0 @@
-
-
-function setAnonPanel() {
-	var e = document.getElementById("ad_content");
-	e.innerHTML = "You are random dude";
-}
--- Zd-web/share/scripts/common/undecorate_link.js
+++
@@ -1,6 +0,0 @@
-
-//a function that will alter the text-decoration CSS inline property to none for the given link, passed by ID when mouseoff
-function undecorate_link(x) {
-        var y = document.getElementById(x);
-        y.className = "nav";
-}
